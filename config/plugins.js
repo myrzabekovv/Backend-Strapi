09322c73242196b5e7bf25f5bb0a345de2970e01
myrzabekovv <akim.myrzabekov06@mail.ru>
2023-05-10 23:56:25 +0600

    backend

---
+++ config/plugins.js
@@ -0,0 +1,36 @@
+module.exports = ({ env }) => ({
+  // ..
+  transformer: {
+    enabled: true,
+    config: {
+      responseTransforms: {
+        removeAttributesKey: true,
+        removeDataKey: true,
+      },
+      requestTransforms: {
+        wrapBodyWithDataKey: true,
+      },
+    },
+  },
+  // ..
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+  "strapi-plugin-populate-deep": {
+    config: {
+      defaultDepth: 3, // Default is 5
+    },
+  },
+});
